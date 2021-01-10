8dd43c7ef1314d5d5650dd2b5ee9b5eb17c6c85d
Edfood <kfed542@gmail.com>
2021-01-11 04:26:51 +0900

    Make limit_time nullable

--- models/user.py
+++ models/user.py
@@ -6,8 +6,8 @@ from models.setting import Base, session
 class User(Base):
     __tablename__ = 'users'
     id = Column('id', String(50), unique=True, primary_key=True)
+    limit_time = Column('limitTime', Integer, nullable=False)
     playtimes = relationship('Playtime', backref='user', cascade='all,delete-orphan')
-    limit_time = Column('limitTime', Integer)
 
     def __repr__(self):
         return f"<User(id='{self.id}', playtime='{self.playtimes}', limit_time='{self.limit_time}')>"
